36b14ac15b637444787a557ef028e7f029602af4
dncworking <dominyka.dobuzinskaite@gmail.com>
2025-11-18 13:16:37 +0200

    add nuotaika.js changes / work done

--- nuotaika.js
+++ nuotaika.js
@@ -1,32 +1,37 @@
 "use strict";
 let a = Number(prompt("Kokia savaites diena?(1-7)"));
 function dayMood(num) {
+  let message = "";
   switch (num) {
     case 1:
-      console.log("Pirmadienis. Stiprios kavos prasau.");
+      message = "Pirmadienis. Stiprios kavos prasau.";
       break;
     case 2:
-      console.log("Antradienis. Jau geriau.");
+      message = "Antradienis. Jau geriau.";
       break;
     case 3:
-      console.log("Treciadienis. Puse kelio!");
+      message = "Treciadienis. Puse kelio!";
       break;
     case 4:
-      console.log("Ketvirtadienis. Dar truputi.");
+      message = "Ketvirtadienis. Dar truputi.";
       break;
     case 5:
-      console.log("Penktadienis. Sirdis dainuoja!");
+      message = "Penktadienis. Sirdis dainuoja!";
       break;
     case 6:
-      console.log("Sestadienis. Galima ilgiau pamiegoti.");
+      message = "Sestadienis. Galima ilgiau pamiegoti.";
       break;
     case 7:
-      console.log("Sekmadienis. Nieko neveikimo menas");
+      message = "Sekmadienis. Nieko neveikimo menas";
       break;
 
     default:
-      console.log("Tokios dienos nera");
+      message = "Tokios dienos nera";
       break;
   }
+  if (num === 5 || num === 6) {
+    message += " Sventine diena";
+  }
+  console.log(message);
 }
 dayMood(a);
